09b238f569791c5ab4628b97fa466a2c29862bd2
isabelagoncalves1 <47746608+isabelagoncalves1@users.noreply.github.com>
2019-09-11 15:52:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
-<!DOCTYPE html>
+<div id="tudo2">
+    <h2>Seção Administrador</h2>
+    <table class="table">
+        <thead>
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+            <tr >
+                <th class="prod">ID</th>
+                <th class="prod">NOME</th>
+                <th class="prod">Preço</th>
+                <th class="prod">Descrição</th>
+                
 
-        <form action="" method="POST">
+            </tr>
+
+
+        </thead>
+        <?php foreach ($produtos as $produto): ?>
+            <tr>
+ 
+                <td class="descr"><?= $produto['idProduto'] ?></td>
+                <td class="descr"><?= $produto['nome'] ?></td>
+                <td class="descr"><?= $produto['preco'] ?></td>
+                <td class="descr"><?= $produto['descricao'] ?></td>
                 
                 
-        </form>
-        
-        
-        ?>
-    </body>
-</html>
+                <td class="descr"><a href="./produto/ver/<?= $produto['idProduto'] ?>" class="botao02">Ver</a></td>
+                <td class="descr"><a href="./produto/deletar/<?= $produto['idProduto'] ?>" class="botao02">Deletar</a></td>
+                <td class="descr"><a href="./produto/editar/<?= $produto['idProduto'] ?>" class="botao02">Alterar</a></td>
+            </tr>
+
+        <?php endforeach; ?>
+    </table>
+    <a href="./produto/adicionar" class="botao05"> Novo produto</a>
+</div>
